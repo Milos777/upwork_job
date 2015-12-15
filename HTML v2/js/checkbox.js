38fd2f657bb22b5b437dc0504df73529028302df
MilosBjelcevic <milos.bjelcevic@diwanee.com>
2015-12-15 09:41:00 +0100

    first

---
+++ HTML v2/js/checkbox.js
@@ -0,0 +1,32 @@
+﻿!function(a){a.CustomData={elements:a()},a.fn.extend({Custom:function(b){var c=this;a.CustomData.elements=a.CustomData.elements.add(c);var d={customStyleClass:"checkbox",customHeight:"16",enableHover:!1};b=a.extend(d,b);var e=function(){var c=a(this).children("input");c.prop("checked")?a(this).css("backgroundPosition","0px -"+3*b.customHeight+"px"):a(this).css("backgroundPosition","0px -"+b.customHeight+"px")},f=function(){var c=a(this),d=c.children("input");d.prop("checked")&&d.is(":checkbox")?(c.css("backgroundPosition","0px 0px"),d.prop("checked",!1).change()):(d.is(":checkbox")?c.css("backgroundPosition","0px -"+2*b.customHeight+"px"):(c.css("backgroundPosition","0px -"+2*b.customHeight+"px"),a('input[name="'+d.attr("name")+'"]').not(d).parent().css("backgroundPosition","0px 0px")),d.prop("checked",!0).change())},g=function(){a.CustomData.elements.each(function(){var b=a(this);b.prop("checked")?b.parent().css("backgroundPosition","0px -"+2*b.attr("data-height")+"px"):b.parent().css("backgroundPosition","0px 0px")})},h=function(){var b=a(this);b.parent()[b.prop("disabled")?"unbind":"bind"]({mousedown:e,mouseup:f})[b.prop("disabled")?"addClass":"removeClass"]("disabled"),b.prop("disabled")?a("label[for="+b.attr("id")+"]").unbind("mouseenter.label").unbind("mouseout.label"):i(b)},i=function(c){var d=c,e=d.parent();b.enableHover&&!d.prop("disabled")&&a("label[for="+d.attr("id")+"]").bind({"mouseenter.label":function(){d.prop("checked")?e.css("backgroundPosition","0px -"+3*d.attr("data-height")+"px"):e.css("backgroundPosition","0px -"+d.attr("data-height")+"px")},"mouseout.label":function(){d.prop("checked")?e.css("backgroundPosition","0px -"+2*d.attr("data-height")+"px"):e.css("backgroundPosition","0px 0px")}})};return this.each(function(){var c=a(this);if("1"!=c.attr("data-init")){c.attr({"data-init":"1","data-height":b.customHeight}).wrap("<span/>");var d=c.parent().addClass(b.customStyleClass);c.prop("checked")&&d.css("backgroundPosition","0px -"+2*b.customHeight+"px"),i(c),c.bind({change:g,"custom.refresh":h}),c.prop("disabled")?d.addClass("disabled"):d.parent("label").length?d.bind({mousedown:e}):d.bind({mousedown:e,mouseup:f})}})}})}(jQuery);
+
+
+
+$(document).ready(function(){
+    $(".radioY").Custom({
+        customStyleClass:'radio',
+        customHeight:'20',
+        enableHover: true
+    });
+    $(".checkY").Custom({
+        customStyleClass:'check',
+        customHeight:'20',
+        enableHover: true
+    });
+    $(".radioZ").Custom({
+        customStyleClass:'radio2 greyRadBtn',
+        customHeight:'15',
+        enableHover: true
+    });
+    $(".radioQ").Custom({
+        customStyleClass:'radio3 greyRadBtn',
+        customHeight:'15',
+        enableHover: true
+    });
+
+    $(".checkQ").Custom({
+        customStyleClass:'check2',
+        customHeight:'15',
+        enableHover: true
+    });
+});
---
+++ HTML v2/js/script.js
@@ -0,0 +1,132 @@
+
+
+$(document).ready(function(){
+    //tMenu
+	$('.tNav .aMob').click(function(){
+		$(this).next('ul').slideToggle(300).toggleClass('open');
+    return false;
+	});
+
+    //for btnComm
+    $('.btnComm').click(function(){
+        if($(this).hasClass('act')){
+            $(this).removeClass('act').closest('li').find('.commJaw').slideUp();
+        } else{
+            $(this).addClass('act').closest('li').find('.commJaw').slideDown();
+        };
+        return false;
+    });
+
+    
+
+    //colorPicker
+    
+    $('.pickColor').delegate('.lColor:not(.act)', 'click', function() {
+        $('.pickColor .lColor').removeClass('act');
+        $(this).addClass('act');
+    });
+
+    //aSel button
+
+    $('.aSel').click(function(){
+        if($(this).hasClass('act')){
+            $(this).removeClass('act').text('Select');
+        } else{
+            $(this).addClass('act').text('Selected');
+        };
+        return false;
+    });
+
+    //days select
+    $('.days').delegate('a:not(.act)', 'click', function() {
+        $('.days a').removeClass('act');
+        $(this).addClass('act');
+        return false;
+    });
+
+    //tabs select
+    $('ul.uTabs').delegate('li:not(.act)', 'click', function() {
+        $(this).addClass('act').siblings().removeClass('act').parents('div.jaw').find('div.box').hide().eq($(this).index()).fadeIn(150);
+    });
+
+    $('.greyRadBtn').click(function(){
+        $(this).closest('.grayRad').find('.grey').removeClass('grey');
+        $(this).closest('.grayRadIn').addClass('grey');
+        return true;
+    });
+
+    $('ul.usMenu').delegate('li:not(.act)', 'click', function() {
+        $(this).addClass('act').siblings().removeClass('act').closest('div.jaw').find('div.bam').hide().eq($(this).index()).fadeIn(150);
+        
+    });
+
+    //like btn
+    $('.comm .like').click(function(){
+        $(this).toggleClass('act');
+    });
+
+    //interBopards 
+    $('ul.uITab').delegate('li:not(.act)', 'click', function() {
+        $(this).addClass('act').siblings().removeClass('act').closest('div.jaw').find('div.box3').hide().eq($(this).index()).fadeIn(150);
+    });
+
+    //members
+
+    $('.members .iDel').click(function(){
+        $(this).closest('li').remove();
+    });
+
+    // openbox
+    $('.btnComm').click(function(){
+        $(this).parent().find('div').fadeIn(250);
+    });
+    $('.faClose').click(function(){
+        $(this).parent().fadeOut(150);
+    });
+
+    //sort dropdown options
+
+    $('.sortBy .by,.sortBy .to,.sortIt .by,.sortIt .to').click(function(){
+        $(this).parent().find('ul').slideToggle(150);
+    });
+    $('.sortBy .sbDD,.sortIt .sbDD').delegate('li:not(.act)', 'click', function() {
+        $(this).addClass('act').siblings().removeClass('act');
+        $(this).closest('div').addClass('act').find('.to').text($(this).text());
+        $(this).closest('ul').slideToggle(150);
+    });
+
+    // select job grid
+
+    $('.uWorks .btnSelect').click(function(){
+        $(this).closest('li').toggleClass('act');
+        return false;
+    });
+    $('.uWorks2 .btnSelect2').click(function(){
+        $(this).closest('li').toggleClass('act');
+        return false;
+    });
+
+    $('.uWorks2 .btnSelect').click(function(){
+        $(this).closest('li').toggleClass('open');
+        return false;
+    });
+    $('.uWorks2 .btnMin').click(function(){
+        $(this).closest('li').toggleClass('open');
+        return false;
+    });
+    
+
+    $('.seaHead').click(function(){
+        $(this).parent().toggleClass('act');
+        return false;
+    });
+
+    //select design
+    $('.btnTrophy').click(function(){
+        $(this).closest('li').toggleClass('banAct');
+        return false;
+    });
+    
+
+    
+}); 
